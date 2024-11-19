f80aa9d3afe15cddb99273ffee04d3f857dddecd
renka1225 <lotus1225r@gmail.com>
2024-11-19 17:18:31 +0900

    up

--- Uedaが往くRE/Scene/SceneSelect.cpp
+++ Uedaが往くRE/Scene/SceneSelect.cpp
@@ -16,6 +16,7 @@ namespace
 		kContinue,	// 続きから
 		kFirst,		// 初めから
 		kOption,	// オプション
+		kCopyright,	// 権利表記
 		kGameEnd,	// ゲーム終了
 		kSelectNum,	// 選択数
 	};
@@ -23,7 +24,6 @@ namespace
 
 SceneSelect::SceneSelect()
 {
-	//SaveData::GetInstance().Load(); // セーブデータを読み込む
 }
 
 std::shared_ptr<SceneBase> SceneSelect::Update(Input& input)
@@ -51,6 +51,10 @@ std::shared_ptr<SceneBase> SceneSelect::Update(Input& input)
 		else if (m_select == SelectScene::kOption)
 		{
 			return std::make_shared<SceneOption>(shared_from_this());
+		}
+		else if (m_select == SelectScene::kCopyright)
+		{
+
 		}
 		else if (m_select == SelectScene::kGameEnd)
 		{
@@ -82,11 +86,6 @@ void SceneSelect::Draw()
 	DrawString(0, 80, LoadCsv::GetInstance().Get_cMessage("MSG_DEBUG_FIRST"), firstColor);
 	DrawString(0, 100, LoadCsv::GetInstance().Get_cMessage("MSG_DEBUG_OPTION"), optionColor);
 	DrawString(0, 120, LoadCsv::GetInstance().Get_cMessage("MSG_DEBUG_GAMEEND"), gameEndColor);
-
-	// セーブデータを表示する
-	auto saveData = SaveData::GetInstance().GetSaveData();
-	DrawFormatString(0, 180, Color::kColorW, "保存位置(X:%.2f,Y:%.2f,Z:%.2f)", saveData.playerPos.x, saveData.playerPos.y, saveData.playerPos.z);
-	DrawFormatString(0, 200, Color::kColorW, "所持金 %d", saveData.money);
 
 	DrawSceneText("MSG_DEBUG_SELECT"); // シーン名表示
 #endif
